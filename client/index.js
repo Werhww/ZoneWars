de142fc081192c02000900626e2951a257557395
Leo <e-leokle@iskule.no>
2022-10-03 11:39:46 +0200

    begining desing for each menu

--- client/index.js
+++ client/index.js
@@ -1,26 +1,79 @@
-var map = L.map('map').setView([60.4, 5.3], 13);
+function handlePermission() {
+    navigator.permissions.query({ name: 'geolocation' }).then((result) => {
+      if (result.state === 'granted') {
+        report(result.state);
+        geoBtn.style.display = 'none';
+      } else if (result.state === 'prompt') {
+        report(result.state);
+        geoBtn.style.display = 'none';
+        navigator.geolocation.getCurrentPosition(revealPosition,positionDenied,geoSettings);
+      } else if (result.state === 'denied') {
+        report(result.state);
+        geoBtn.style.display = 'inline';
+      }
+      result.addEventListener('change', () => {
+        report(result.state);
+      });
+    });
+}
 
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/dark_nolabels/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-}).addTo(map);
-
-var circle = L.circle([60.4, 5.3], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 100
-}).addTo(map);
-
-map.addEventListener('mousemove', ()=>{
-    circle.setLatLng(map.getCenter())
-});
-
-document.getElementsByClassName( 'leaflet-control-attribution' )[0].style.display = 'none';
+handlePermission()
 
 
-const mapsize = document.getElementById('mapsize')
+function getLocation() {
+    return new Promise((resolve, reject) => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((postion)=>{resolve(postion.coords)});
+        } else {
+            reject()
+        }
+    })
+}
 
-mapsize.oninput = function() {
-    console.log(this.value)
-    circle.setRadius(this.value)
-  }
+
+async function init(){
+    const postion = await getLocation()
+    console.log(postion)
+    const mapPlacement = L.map('mapPlacement').setView([postion.latitude, postion.longitude], 13)
+
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/dark_nolabels/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+    }).addTo(mapPlacement)
+
+    var circle = L.circle([60.4, 5.3], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 100
+    }).addTo(mapPlacement)
+
+    mapPlacement.addEventListener('mousemove', ()=>{
+        circle.setLatLng(mapPlacement.getCenter())
+    })
+
+    const mapsize = document.getElementById('mapSize')
+
+    mapsize.oninput = function() {
+        circle.setRadius(this.value)
+    }
+
+
+    const gameMap = L.map('gameMap').setView([60.4, 5.3], 13);
+
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/dark_nolabels/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+    }).addTo(gameMap);
+
+    var gameCircle = ()=>{
+        var circle = L.circle([60.4, 5.3], {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: 100
+        }).addTo(gameMap);
+    }
+
+    document.getElementsByClassName( 'leaflet-control-attribution' )[0].style.display = 'none';
+}
+
+init()
