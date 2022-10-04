ad1e5beb70314ed168a028916166ac621341c729
Leo <e-leokle@iskule.no>
2022-10-04 10:23:48 +0200

    Update

--- client/_global/buttons.js
+++ client/_global/buttons.js
@@ -1,11 +1,25 @@
+//Main Screen Buttons
 const creatGamebtn = document.getElementById('creatGamebtn')
 const joinGamebtn = document.getElementById('joinGamebtn')
 const settingbtn = document.getElementById('settingbtn')
 
+// Menus
 const mainScreen = document.getElementById('mainScreen')
 const creatGameMenu = document.getElementById('creatGameMenu')
+const creatGameMapPlacement = document.getElementById('')
 
 creatGamebtn.addEventListener('click', ()=>{
     mainScreen.style.display = 'none'
     creatGameMenu.style.display = 'flex'
 })
+
+function fetchHostName(){
+    const userName = document.forms['hostNameForm']['hostName'].value;
+    if (userName == "" || userName.includes(" ")){
+        alert("Name must be filled out");
+        return false;
+    }
+    console.log(userName)
+    return false
+}
+
