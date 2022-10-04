b8b9808ac32b002d9901cd4c379d1d20c602d427
Leo <e-leokle@iskule.no>
2022-10-04 12:29:36 +0200

    update

--- client/_global/buttons.js
+++ client/_global/buttons.js
@@ -7,6 +7,11 @@ const settingbtn = document.getElementById('settingbtn')
 const mainScreen = document.getElementById('mainScreen')
 const creatGameMenu = document.getElementById('creatGameMenu')
 const creatGameMapPlacement = document.getElementById('creatGameMapPlacement')
+const creatGameSettings = document.getElementById('creatGameSettings')
+
+//Button
+const mapback = document.getElementById('mapBack')
+const mapnext = document.getElementById('mapNext')
 
 creatGamebtn.addEventListener('click', ()=>{
     mainScreen.style.display = 'none'
@@ -24,7 +29,30 @@ function fetchHostName(){
     creatGameMenu.style.display = 'none'
     creatGameMapPlacement.style.display = 'flex'
 
+    return false
+}
+
+mapback.addEventListener('click', ()=>{
+    creatGameMenu.style.display = 'flex'
+    creatGameMapPlacement.style.display = 'none'
+
+})
+
+mapnext.addEventListener('click', ()=>{
+    creatGameMapPlacement.style.display = 'none'
+    creatGameSettings.style.display = 'flex'
+
+})
+
+function gameback(){
+    creatGameSettings.style.display = 'none'
+    creatGameMapPlacement.style.display = 'flex'
 
     return false
 }
 
+function gameStart(){
+    window.location.href = "./game";
+
+    return false
+}
