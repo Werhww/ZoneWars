98d76be303f4af6a5ada5b21ad26fa7979b003ac
PowerKuu <hkleven6@outlook.com>
2022-10-04 09:42:16 +0200

    Sync

--- client/index.js
+++ client/index.js
@@ -1,113 +1,10 @@
-//Gets position permission
-function GetPerms(){
-    const geolocationOptions = {
-        enableHighAccuracy: true,
-        maximumAge: 10000,
-        timeout: 5000,
-    }
-    return new Promise((resolve, reject) => {
-          
-        if ("geolocation" in navigator) {
-            navigator.geolocation.getCurrentPosition(
-            () => {
-                resolve()
-            },
-            () => {
-                alert("U need to enable geolocation!")
-                reject()
-            },
-            geolocationOptions
-            )
-        } else {
-            reject()
-            console.log("Browser does not support the Geolocation API")
-        }
-    })
-}
-
-//Function with gets players latitude and longitude
-function getLocation() {
-    return new Promise((resolve, reject) => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((postion)=>{resolve(postion.coords)}, ()=>{},{
-                enableHighAccuracy: true,
-                timeout: 7000,
-                maximumAge: 0
-            });
-
-        } else {
-            reject()
-        }
-    })
-}
-
-// Makes tile 1px larger to hide white line
-(function(){
-    var originalInitTile = L.GridLayer.prototype._initTile
-    L.GridLayer.include({
-        _initTile: function (tile) {
-            originalInitTile.call(this, tile);
-
-            var tileSize = this.getTileSize();
-
-            tile.style.width = tileSize.x + 1 + 'px';
-            tile.style.height = tileSize.y + 1 + 'px';
-        }
-    });
-})()
 
 
-async function init(){
-    await GetPerms().catch(()=>{})
-    ///const postion = await getLocation()
 
-    const mapPlacement = L.map('mapPlacement').setView([55, 55], 13)
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/dark_nolabels/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-    }).addTo(mapPlacement)
-    document.getElementsByClassName( 'leaflet-control-attribution' )[0].style.display = 'none';
 
-    var circle = L.circle([60.4, 5.3], {
-        color: 'red',
-        fillColor: '#f03',
-        fillOpacity: 0.5,
-        radius: 100
-    }).addTo(mapPlacement)
 
-    mapPlacement.addEventListener('move', ()=>{
-        circle.setLatLng(mapPlacement.getCenter())
-    })
-    
-    const mapsize = document.getElementById('mapSize')
 
-    mapsize.oninput = function() {
-        circle.setRadius(this.value)
-    }
-    
-
-    const gameMap = L.map('gameMap').setView([60.4, 5.3], 13);
-
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/dark_nolabels/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-    }).addTo(gameMap);
-
-    var gameCircle = ()=>{
-        var circle = L.circle([60.4, 5.3], {
-            color: 'red',
-            fillColor: '#f03',
-            fillOpacity: 0.5,
-            radius: 100
-        }).addTo(gameMap);
-    }
-
-    const onPosIcon = document.getElementById('onPosIcon')
-
-    onPosIcon.addEventListener('click', ()=>{
-        mapPlacement.setView(new L.LatLng(40.737, -73.923), 8);
-    })
-
-}
 const h = document.getElementById("hTime")
 function showVal(val){
     if (val < 200000){
@@ -127,4 +24,3 @@ function showVal(val){
 }
 
 showVal(300000)
-init()
