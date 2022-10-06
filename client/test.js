7203e541897bc93515f751ea56325c3aee322550
PowerKuu <hkleven6@outlook.com>
2022-10-06 22:52:07 +0200

    update

--- client/test.js
+++
@@ -1,5 +0,0 @@
-const mapPlacement = L.map('mapPlacement').setView([53, 55], 13)
-
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/dark_nolabels/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-}).addTo(mapPlacement)
